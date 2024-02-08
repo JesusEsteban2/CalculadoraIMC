5cd8167907f806ba672da67e5ce2e17dbb6f25e2
mananas <jesus.esteban@outlook.com>
2024-02-08 12:46:52 +0100

    First Version

---
+++ app/src/main/java/com/example/calculadoraimc/activities/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.calculadoraimc.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatButton
+import com.example.calculadoraimc.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val userEditText:EditText =findViewById(R.id.edittAltura)
+
+        val btnCalc:AppCompatButton=findViewById(R.id.btn_Calcular)
+
+        btnCalc.setOnClickListener({userEditText.setText("Has pulsado calcular")})
+    }
+}
